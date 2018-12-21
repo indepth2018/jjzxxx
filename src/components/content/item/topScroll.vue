0a392550b81fbfaaf06351804d2410df3126676c
indepth2018 <875439792@qq.com>
2018-12-22 00:21:04 +0800

    fix:top word srcoll

--- src/components/content/item/topScroll.vue
+++ src/components/content/item/topScroll.vue
@@ -1,10 +1,13 @@
 <style lang='stylus'>
 .scrollWord
     height 25px
+    line-height 25px
     overflow hidden
     white-space nowrap
 
     ul
+        display inline-block
+
         li
             display inline-block
             margin-right 200px
@@ -13,11 +16,12 @@
 
 <template>
     <div class="scrollWord" ref="scrollWord" @click="test">
-        <ul ref="scrollUl">
+        <ul ref="scrollUl1">
             <li>好消息好消息1111111111111111111</li>
             <li>网页开始动工啦呀呀呀111111</li>
             <li>12222222222222222222</li>
         </ul>
+        <ul ref="scrollUl2"></ul>
     </div>
 </template>
 
@@ -33,14 +37,19 @@ export default {
         test() {
             console.log(this.$refs.scrollWord.offsetWidth);
             console.log(this.$refs.scrollWord.scrollLeft);
-            console.log(this.$refs.scrollUl.offsetWidth);
+
+            console.log(this.$refs.scrollUl2.innerHTML);
         }
     },
     mounted() {
         let scrollWord = this.$refs.scrollWord;
-        let scrollUl = this.$refs.scrollUl;
+        let scrollUl1 = this.$refs.scrollUl1;
+        let scrollUl2 = this.$refs.scrollUl2;
+        scrollUl2.innerHTML = scrollUl1.innerHTML;
         function scrollUp() {
-            scrollWord.scrollLeft++;
+            scrollWord.scrollLeft >= scrollUl1.offsetWidth
+                ? (scrollWord.scrollLeft = 0)
+                : scrollWord.scrollLeft++;
         }
         let scrollTime = setInterval(scrollUp, 50);
         scrollWord.onmouseover = () => {
